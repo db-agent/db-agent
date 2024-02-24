ec6b056625acb8eb4565f30ce1181af93a11efd6
Chandan Kumar <chandank@skucaster.com>
2024-02-23 20:04:11 -0500

    working demo app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
+import sys,os
+from pathlib import Path
+
+# Add the project root directory to the Python path
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root))
+
 from config_loader import ConfigLoader
 from db.db_manager import DatabaseManager
 from services.db_service import DBService
 from services.openai_service import OpenAIInterface
+
+from langchain.chains import create_sql_query_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.utilities import SQLDatabase
 
 def main():
     # Load configurations
@@ -15,20 +26,20 @@ def main():
     # Initialize services
     db_service = DBService(db_manager)
     openai_service = OpenAIInterface(openai_api_key)
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    db = SQLDatabase.from_uri(
+    f"mysql://{db_config['user']}:"
+    f"{db_config['password']}@"
+    f"{db_config['host']}:3306/"
+    f"{db_config['database']}")
 
-    # Example application flow
-    # 1. Get a query from the user
-    user_query = input("Enter your query: ")
-
-    # 2. Use OpenAI service to generate a response
-    response = openai_service.generate_text(user_query)
-
-    # 3. Store the query and response in the database
-    db_service.insert_response(user_query, response)
-
-    # 4. Fetch and display the response
-    fetched_response = db_service.fetch_response_by_query(user_query)
-    print("Response from OpenAI:", fetched_response)
+    print(db.dialect)
+    print(db.get_usable_table_names())
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    chain = create_sql_query_chain(llm, db)
+    response = chain.invoke({"question": "Show top 10 Part number from inventory history"})
+    print(response)
+    print(db.run(response))
 
     # Clean up resources
     db_manager.close()
